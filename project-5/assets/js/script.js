f6451584e06f243e01bd290dc2321cecab878a8f
MahmudE14 <mahmud.e14@gmail.com>
2022-11-05 15:29:20 +0600

    project-5: start

--- project-5/assets/js/script.js
+++ project-5/assets/js/script.js
@@ -11,22 +11,37 @@ let ravens = [];
 
 class Raven {
     constructor() {
-        this.width = 100;
-        this.height = 50;
+        this.spriteWidth = 270;
+        this.spriteHeight = 195;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x = CANVAS_WIDTH;
         this.y = Math.random() * (CANVAS_HEIGHT - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 + 2.5;
+        this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = "./assets/images/raven.png";
+        this.frame = 0;
+        this.maxFrame = 5;
+        this.timeSinceFlap = 0;
+        this.flapInterval = 100;
     }
 
-    update() {
+    update(deltatime) {
         this.x -= this.directionX;
+        this.frame = this.frame % this.maxFrame;
+        this.frame++;
+        if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        if (this.timeSinceFlap > this.flapInterval) this.timeSinceFlap = 0;
         return this;
     }
 
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        // ctx.fillRect(this.image, this.x, this.y, this.width, this.height, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -42,7 +57,8 @@ function animate(timestamp) {
         timeToNextRaven = 0;
     }
 
-    [...ravens].forEach(raven => raven.update().draw());
+    [...ravens].forEach(raven => raven.update(deltatime).draw());
+    ravens = ravens.filter(raven => !raven.markedForDeletion)
 
     requestAnimationFrame(animate)
 }
