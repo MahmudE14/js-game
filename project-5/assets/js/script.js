37d52ec5d9c7a4079512684851a4cb9d36bd5d74
MahmudE14 <mahmud.e14@gmail.com>
2022-11-07 12:18:30 +0600

    project-5: add sprite movement and vertical bounce

--- project-5/assets/js/script.js
+++ project-5/assets/js/script.js
@@ -1,10 +1,10 @@
 const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvas1'));
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = window.innerWidth;
-const CANVAS_HEIGHT = window.innerHeight;
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
 
 let timeToNextRaven = 0;
-let ravenInterval = 500;
+let ravenInterval = 800;
 let lastTime = 0;
 
 let ravens = [];
@@ -12,30 +12,37 @@ let ravens = [];
 class Raven {
     constructor() {
         this.spriteWidth = 270;
-        this.spriteHeight = 195;
-        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.2 + 0.2;
         this.width = this.spriteWidth * this.sizeModifier;
         this.height = this.spriteHeight * this.sizeModifier;
         this.x = CANVAS_WIDTH;
         this.y = Math.random() * (CANVAS_HEIGHT - this.height);
         this.directionX = Math.random() * 5 + 3;
-        this.directionY = Math.random() * 5 + 2.5;
+        this.directionY = Math.random() * 5 - 2.5;
         this.markedForDeletion = false;
         this.image = new Image();
         this.image.src = "./assets/images/raven.png";
         this.frame = 0;
-        this.maxFrame = 5;
+        this.maxFrame = 4;
         this.timeSinceFlap = 0;
-        this.flapInterval = 100;
+        this.flapInterval = Math.random() * 50 + 50;
     }
 
     update(deltatime) {
+        if (this.y < 0 || this.y > CANVAS_HEIGHT - this.height) {
+            this.directionY = this.directionY * -1
+        }
         this.x -= this.directionX;
-        this.frame = this.frame % this.maxFrame;
-        this.frame++;
+        this.y += this.directionY;
         if (this.x < 0 - this.width) this.markedForDeletion = true;
         this.timeSinceFlap += deltatime;
-        if (this.timeSinceFlap > this.flapInterval) this.timeSinceFlap = 0;
+        if (this.timeSinceFlap > this.flapInterval) {
+            if (this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;
+            this.timeSinceFlap = 0;
+        }
+
         return this;
     }
 
@@ -44,7 +51,6 @@ class Raven {
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
-
 
 function animate(timestamp) {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -62,4 +68,4 @@ function animate(timestamp) {
 
     requestAnimationFrame(animate)
 }
-// animate(0);
+animate(0);
