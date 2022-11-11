32517d7e2ee3d0d26c149a245a98adf6ddc379ae
MahmudE14 <mahmud.e14@gmail.com>
2022-11-11 12:04:25 +0600

    project-6: add curve movememnt pattern to ghost enemy type

--- project-6/assets/js/script.js
+++ project-6/assets/js/script.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function () {
       this.width = width;
       this.height = height;
       this.enemies = [];
-      this.enemyInterval = 100;
+      this.enemyInterval = 500;
       this.enemyTimer = 0;
       this.enemyTipes = ["worm", "ghost"];
     }
@@ -45,7 +45,7 @@ window.addEventListener("load", function () {
 
       // e.g.: this.enemies.push(new Worm(this));
       this.enemies.push(new enemyTypeEnum[randomEnemy](this));
-      this.enemies.sort((a, b) => a.y - b.y);
+    //   this.enemies.sort((a, b) => a.y - b.y);
     }
   }
 
@@ -80,7 +80,7 @@ window.addEventListener("load", function () {
       this.width = this.spriteWidth/2;
       this.height = this.spriteHeight/2;
       this.x = this.game.width;
-      this.y = Math.random() * this.game.height;
+      this.y = this.game.height - this.height;
       this.vx = Math.random() * 0.1 + 0.1;
     }
   }
@@ -94,8 +94,24 @@ window.addEventListener("load", function () {
       this.width = this.spriteWidth/2;
       this.height = this.spriteHeight/2;
       this.x = this.game.width;
-      this.y = Math.random() * this.game.height;
+      this.y = Math.random() * this.game.height * 0.6;
       this.vx = Math.random() * 0.2 + 0.1;
+      this.angle = 0;
+      this.curve = Math.random() * 3;
+    }
+
+    update(deltaTime) {
+        super.update(deltaTime);
+        this.y += Math.sin(this.angle) * this.curve;
+        this.angle += 0.04;
+        return this;
+    }
+
+    draw() {
+        ctx.save();
+        ctx.globalAlpha = 0.5;
+        super.draw(ctx);
+        ctx.restore();
     }
   }
 
