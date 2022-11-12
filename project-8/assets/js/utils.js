fe93fa49d18a69d8eab401bc51c398ea0ca0f568
MahmudE14 <mahmud.e14@gmail.com>
2022-11-13 01:47:52 +0600

    project-8: init to complete. (note: Used state design pattern)

---
+++ project-8/assets/js/utils.js
@@ -0,0 +1,10 @@
+/**
+ * Draw status text
+ * @param {CanvasRenderingContext2D} context 
+ * @param {*} input 
+ */
+export function drawStatusText(context, input, player) {
+    context.font = "28px Helvetica";
+    context.fillText("Last input: " + input.lastKey, 20, 50);
+    context.fillText("Active state: " + player.currentState.state, 20, 90);
+}
