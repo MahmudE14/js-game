c38e43d8eb5281c459a8cc1a63262deed87a0458
MahmudE14 <mahmud.e14@gmail.com>
2022-11-13 23:12:55 +0600

    project-9: add fying enemy behaviour

--- project-9/assets/js/enemies.js
+++ project-9/assets/js/enemies.js
@@ -5,31 +5,74 @@ class Enemy {
         this.fps = 20;
         this.frameInterval = 1000/this.fps;
         this.frameTimer = 0;
+        this.markedForDeletion = false;
     }
 
-    update() {
-        // 
+    update(deltaTime) {
+        // movement
+        this.x -= this.speedX + this.game.speed;
+        this.y += this.speedY;
+        if (this.frameTimer > this.frameInterval) {
+            this.frameTimer++;
+            this.frameX = this.frameX < this.maxFrame ? this.frameX + 1 : 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
+
+        // check if off-screen
+        if (this.x + this.width < 0) this.markedForDeletion = true;
     }
 
     draw(/** @type {CanvasRenderingContext2D} */ context) {
+        context.drawImage(
+            this.image,
+            this.frameX * this.width,
+            0,
+            this.width,
+            this.height,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        )
+    }
+}
+
+export class FlyingEnemy extends Enemy {
+    constructor(game) {
+        super();
+        this.game = game;
+        this.width = 60;
+        this.height = 44;
+        this.x = this.game.width + Math.random() * this.game.width * 0.5;
+        this.y = Math.random() * this.game.height * 0.5;
+        this.speedX = Math.random() + 1;
+        this.speedY = 0;
+        this.maxFrame = 5;
+        this.image = enemy_fly;
+        this.angle = 0;
+        this.va = Math.random() * 0.1 + 0.1;
+    }
+
+    update(deltaTime) {
+        super.update(deltaTime);
+        this.angle += this.va;
+        this.y += Math.sin(this.angle);
+    }
+}
+
+export class RunningEnemy extends Enemy {
+    constructor(game) {
         // 
     }
 }
 
-class FlyingEnemy extends Enemy {
+export class ClimbingEnemy extends Enemy {
     constructor() {
         // 
     }
 }
 
-class RunningEnemy extends Enemy {
-    constructor() {
-        // 
-    }
-}
-
-class ClimbingEnemy extends Enemy {
-    constructor() {
-        // 
-    }
-}
+// enemy_fly
+// enemy_plant
+// enemy_spider_big
--- project-9/assets/js/script.js
+++ project-9/assets/js/script.js
@@ -1,6 +1,11 @@
 import Player from "./player.js";
 import InputHandler from "./input.js";
 import Background from "./background.js";
+import {
+    FlyingEnemy,
+    RunningEnemy,
+    ClimbingEnemy,
+} from "./enemies.js";
 
 window.addEventListener("load", function () {
     // loading.style.display = "none";
@@ -22,17 +27,39 @@ window.addEventListener("load", function () {
             this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
         }
 
         update(deltaTime) {
             this.background.update();
             this.player.update(this.input.keys, deltaTime);
+
+            // handle enemies
+            if (this.enemyTimer > this.enemyInterval) {
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
+
+            this.enemies.forEach(enemy => {
+                enemy.update(deltaTime);
+                if (enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+            });
+
             return this;
         }
 
         draw(/** @type {CanvasRenderingContext2D} */ context) {
             this.background.draw(context);
             this.player.draw(context);
+            this.enemies.forEach(enemy => enemy.draw(context));
+        }
+
+        addEnemy() {
+            this.enemies.push(new FlyingEnemy(this));
         }
     }
 
@@ -42,9 +69,7 @@ window.addEventListener("load", function () {
     function animate(timestamp) {
         const deltaTime = timestamp - lastTime;
         lastTime = timestamp;
-
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
         game.update(deltaTime).draw(ctx);
 
         requestAnimationFrame(animate);
